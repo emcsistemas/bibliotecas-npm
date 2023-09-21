b7c9217b6c8c6113ed2707813821a774dd41d4d3
Léo Fernandes <leandroleobh@gmail.com>
2023-09-21 16:51:51 -0300

    biblioteca atualizada

--- native-ui/src/components/Avatar/Avatar.tsx
+++ native-ui/src/components/Avatar/Avatar.tsx
@@ -2,6 +2,7 @@ import { Image, StyleSheet } from 'react-native'
 import Box from '../Box/Box'
 import { styles, makeBaseImageStyle } from '../../styles/styles'
 import { CustomImageProps } from '../../styles/ui-components.types'
+import { dimensionCalculate } from '../../styles/ui-components.util'
 
 const Avatar = (props: CustomImageProps) => {
   const containerStyles = StyleSheet.compose(
@@ -11,7 +12,12 @@ const Avatar = (props: CustomImageProps) => {
 
   return (
     <Box style={containerStyles}>
-      <Image alt={props.alt} source={{ uri: props.source.uri }} />
+      <Image
+        resizeMode='contain'
+        style={{width: dimensionCalculate(props.w), height: dimensionCalculate(props.h)}}
+        alt={props.alt}
+        source={{ uri: props.source.uri }}
+      />
     </Box>
   )
 }

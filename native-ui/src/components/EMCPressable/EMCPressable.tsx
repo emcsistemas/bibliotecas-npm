ee4c1cf20ca598e8c377c30eeed8373e7410f6b3
Léo Fernandes <leandroleobh@gmail.com>
2024-02-08 10:44:52 -0300

    bibliotecas atualizadas

---
+++ native-ui/src/components/EMCPressable/EMCPressable.tsx
@@ -0,0 +1,46 @@
+import { Pressable, View } from 'react-native'
+import { CustomPressableProps } from '../../styles'
+import { dimensionCalculate } from '../../styles/styles.util'
+
+import Consts from '../../styles/Consts'
+import { forwardRef } from 'react'
+
+const EMCPressable = (props: CustomPressableProps, ref: any) => {
+  return (
+    <Pressable
+      ref={ref || undefined}
+      style={({ pressed }) => [
+        {
+          opacity: props.noPressEffect
+            ? 1
+            : pressed
+            ? Consts.DEFAULT_OPACITY_CLICK
+            : 1,
+          position: props.position,
+          left: props.left,
+          top: props.top,
+          right: props.right,
+          bottom: props.bottom,
+          flex: props.flex,
+          justifyContent: props.justify,
+          alignItems: props.align,
+          width: dimensionCalculate(props.w),
+          height: dimensionCalculate(props.h),
+          margin: dimensionCalculate(props.m),
+          marginHorizontal: dimensionCalculate(props.mx),
+          marginVertical: dimensionCalculate(props.my),
+          marginLeft: dimensionCalculate(props.ml),
+          marginRight: dimensionCalculate(props.mr),
+          marginTop: dimensionCalculate(props.mt),
+          marginBottom: dimensionCalculate(props.mb),
+          zIndex: props.zIndex,
+        },
+      ]}
+      {...props}
+    >
+      {props.children}
+    </Pressable>
+  )
+}
+
+export default forwardRef<View, CustomPressableProps>(EMCPressable)

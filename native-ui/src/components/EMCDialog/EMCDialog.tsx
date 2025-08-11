a16d34c871c9279f7957084b6999916f7005ef7c
leofernandesbh <leandroleobh@gmail.com>
2025-08-11 15:00:37 -0300

    update: native-ui atualizada para React 19

--- native-ui/src/components/EMCDialog/EMCDialog.tsx
+++ native-ui/src/components/EMCDialog/EMCDialog.tsx
@@ -1,35 +1,41 @@
-import { useState } from "react"
-import { Keyboard, Modal, ModalProps, TouchableWithoutFeedback } from "react-native"
-import { FontAwesome } from "@expo/vector-icons"
-import EMCBox from "../EMCBox"
-import EMCButton from "../EMCButton"
-import EMCDivider from "../EMCDivider"
-import EMCHStack from "../EMCHStack"
-import EMCIcon from "../EMCIcon"
-import EMCText from "../EMCText"
-import EMCTextArea from "../EMCTextArea"
-import showToast from "../EMCToast"
-import EMCVStack from "../EMCVStack"
-import { Colors } from "../../theme"
-import Consts from "../../styles/Consts"
+import { useState } from "react";
+import {
+  Keyboard,
+  Modal,
+  ModalProps,
+  TouchableWithoutFeedback,
+} from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+import EMCBox from "../EMCBox";
+import EMCButton from "../EMCButton";
+import EMCDivider from "../EMCDivider";
+import EMCHStack from "../EMCHStack";
+import EMCIcon from "../EMCIcon";
+import EMCText from "../EMCText";
+import EMCTextArea from "../EMCTextArea";
+import showToast from "../EMCToast";
+import EMCVStack from "../EMCVStack";
+import { Colors } from "../../theme";
+import Consts from "../../styles/Consts";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 interface EMCDialogProps extends ModalProps {
   dialogType:
-    | 'information'
-    | 'alert'
-    | 'error'
-    | 'success'
-    | 'confirmation'
-    | 'request'
-  message: string
-  minResponseLength?: number
-  alertMessageEmptyResponse?: string
-  noHeader?: boolean
-  showBorder?: boolean
-  isTablet?: boolean
-  handleConfirm?(): void
-  handleConfirmRequest?(requestData: string): void
-  handleClose(): void
+    | "information"
+    | "alert"
+    | "error"
+    | "success"
+    | "confirmation"
+    | "request";
+  message: string;
+  minResponseLength?: number;
+  alertMessageEmptyResponse?: string;
+  noHeader?: boolean;
+  showBorder?: boolean;
+  isTablet?: boolean;
+  handleConfirm?(): void;
+  handleConfirmRequest?(requestData: string): void;
+  handleClose(): void;
 }
 
 const EMCDialog = ({
@@ -45,43 +51,41 @@ const EMCDialog = ({
   handleClose,
   ...rest
 }: EMCDialogProps) => {
-  const [requestData, setRequestData] = useState('')
+  const [requestData, setRequestData] = useState("");
+  const insets = useSafeAreaInsets();
 
   function confirmRequest() {
     if (requestData.trim().length === 0) {
-      showToast(alertMessageEmptyResponse ?? 'Por favor, informe os dados solicitados', 'alert')
-      return
+      showToast(
+        alertMessageEmptyResponse ?? "Por favor, informe os dados solicitados",
+        "alert",
+        undefined,
+        insets.top
+      );
+      return;
     }
 
     if (minResponseLength && requestData.trim().length < minResponseLength) {
       showToast(
         `O texto deve conter pelo menos ${minResponseLength.toString()} caracteres`,
-        'alert',
-      )
-      return
+        "alert"
+      );
+      return;
     }
 
     if (handleConfirmRequest) {
-      handleConfirmRequest(requestData)
+      handleConfirmRequest(requestData);
     }
   }
 
   return (
-    <Modal
-      transparent={true}
-      animationType='fade'
-      {...rest}
-    >
+    <Modal transparent={true} animationType="fade" {...rest}>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <EMCVStack flex={1}>
-          <EMCVStack
-            flex={1}
-            justify='center'
-            px={isTablet ? '20%' : 6}
-          >
+          <EMCVStack flex={1} justify="center" px={isTablet ? "20%" : 6}>
             <EMCVStack
               bg={Colors.light[50]}
-              rounded='md'
+              rounded="md"
               p={4}
               pt={noHeader ? 0 : 4}
               bColor={Colors.gray[800]}
@@ -96,63 +100,59 @@ const EMCDialog = ({
                   <EMCIcon
                     as={FontAwesome}
                     name={
-                      dialogType === 'alert'
-                        ? 'warning'
-                        : dialogType === 'error'
-                        ? 'times-circle'
-                        : dialogType === 'success'
-                        ? 'check-circle'
-                        : dialogType === 'confirmation' ||
-                          dialogType === 'request'
-                        ? 'question-circle'
-                        : 'info-circle'
+                      dialogType === "alert"
+                        ? "warning"
+                        : dialogType === "error"
+                        ? "times-circle"
+                        : dialogType === "success"
+                        ? "check-circle"
+                        : dialogType === "confirmation" ||
+                          dialogType === "request"
+                        ? "question-circle"
+                        : "info-circle"
                     }
                     color={
-                      dialogType === 'alert' || dialogType === 'error'
+                      dialogType === "alert" || dialogType === "error"
                         ? Colors.danger[600]
-                        : dialogType === 'success'
+                        : dialogType === "success"
                         ? Colors.success[700]
                         : Colors.blue[400]
                     }
-                    size={dialogType === 'alert' ? 6 : 7}
-                    mr={dialogType === 'alert' ? 3 : 2}
+                    size={dialogType === "alert" ? 6 : 7}
+                    mr={dialogType === "alert" ? 3 : 2}
                   />
-                  <EMCText
-                    mb={0}
-                    fSize={isTablet ? 'lg' : 'md'}
-                    fWeight='bold'
-                  >
-                    {dialogType === 'alert'
-                      ? 'Atenção!'
-                      : dialogType === 'error'
-                      ? 'Ops!'
-                      : dialogType === 'success'
-                      ? 'Sucesso'
-                      : dialogType === 'confirmation' ||
-                        dialogType === 'request'
-                      ? 'Confirmação'
-                      : 'Informação'}
+                  <EMCText mb={0} fSize={isTablet ? "lg" : "md"} fWeight="bold">
+                    {dialogType === "alert"
+                      ? "Atenção!"
+                      : dialogType === "error"
+                      ? "Ops!"
+                      : dialogType === "success"
+                      ? "Sucesso"
+                      : dialogType === "confirmation" ||
+                        dialogType === "request"
+                      ? "Confirmação"
+                      : "Informação"}
                   </EMCText>
                 </EMCHStack>
               )}
               <EMCBox pt={2}>
                 <EMCText
-                  fSize={isTablet ? 'md' : 'sm'}
+                  fSize={isTablet ? "md" : "sm"}
                   wordWrap
-                  fWeight={noHeader ? 'bold' : 'normal'}
+                  fWeight={noHeader ? "bold" : "normal"}
                 >
                   {message}
                 </EMCText>
                 {handleConfirmRequest && (
                   <EMCTextArea
-                    w='full'
+                    w="full"
                     h={isTablet ? 25 : 20}
                     mt={2}
                     autoFocus
                     maxLength={150}
                     maxFontSizeMultiplier={Consts.MAX_ACCESSIBILITY_MULTIPLIER}
-                    fSize={isTablet ? 'md' : 'sm'}
-                    placeholder='Motivo'
+                    fSize={isTablet ? "md" : "sm"}
+                    placeholder="Motivo"
                     autoCorrect={false}
                     bColor={Colors.gray[500]}
                     onChangeText={setRequestData}
@@ -161,24 +161,21 @@ const EMCDialog = ({
                 <EMCDivider my={4} />
               </EMCBox>
               {handleConfirm || handleConfirmRequest ? (
-                <EMCHStack
-                  align='center'
-                  justify='center'
-                >
+                <EMCHStack align="center" justify="center">
                   <EMCButton
                     mr={3}
                     isDialog
-                    variant='outline'
+                    variant="outline"
                     ml={0}
-                    title='Cancelar'
+                    title="Cancelar"
                     onPress={() => handleClose()}
                     isTablet={isTablet}
                   />
                   <EMCButton
                     ml={3}
                     isDialog
-                    variant={handleConfirmRequest ? 'red' : 'blue'}
-                    title={handleConfirmRequest ? 'Confirmar' : 'Sim'}
+                    variant={handleConfirmRequest ? "red" : "blue"}
+                    title={handleConfirmRequest ? "Confirmar" : "Sim"}
                     onPress={() =>
                       handleConfirm ? handleConfirm() : confirmRequest()
                     }
@@ -186,11 +183,11 @@ const EMCDialog = ({
                   />
                 </EMCHStack>
               ) : (
-                <EMCBox align='center'>
+                <EMCBox align="center">
                   <EMCButton
                     isDialog
                     ml={0}
-                    title='OK'
+                    title="OK"
                     onPress={() => handleClose()}
                     isTablet={isTablet}
                   />
@@ -201,7 +198,7 @@ const EMCDialog = ({
         </EMCVStack>
       </TouchableWithoutFeedback>
     </Modal>
-  )
-}
+  );
+};
 
-export default EMCDialog
+export default EMCDialog;
